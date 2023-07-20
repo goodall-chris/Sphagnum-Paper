20362e6d7b9962fdb66d1d7db0d2ed1af0351cb1
Chris Goodall <67712136+goodall-chris@users.noreply.github.com>
2023-07-20 17:03:44 -0400

    Update regression-gams for chlorophyll fluorescence data 04-15-2023.R

--- Data and R scripts for Sphagnum manuscript/Chlorophyll Fluorescence/Chlorophyll fluorescence pairwise comparisons/regression-gams for chlorophyll fluorescence data 04-15-2023.R
+++ Data and R scripts for Sphagnum manuscript/Chlorophyll Fluorescence/Chlorophyll fluorescence pairwise comparisons/regression-gams for chlorophyll fluorescence data 04-15-2023.R
@@ -2,11 +2,12 @@
 #chris goodall 02/08/22
 #updated with linear models 02/20/22
 #round by 50 -- 06-03-2023
+#check the optimization -- 07-19-2023
 
 #packages 
 library(readr)
 library(mgcv)
-library(visibly)
+library(MASS)
 library(reshape)
 
 #import data
@@ -19,7 +20,6 @@ d1$PPFD <- round_any(d1$PPFD, 50) #round to nearest increment of 50 for PPFD
 
 #Subsetting to 725 PAR 
 d1 <- d1[d1$PPFD > 749 & d1$PPFD < 751,] #subsetting to 750. 
-
 
 #cat(paste(colnames(d1), collapse=', '))
 
@@ -68,12 +68,12 @@ l <- lmp(Jtot_lm)
 
 #design function to extract values from GAM
 lmp.2 <- function (modelobject) {
-  d<- summary(modelobject)[14] #explained deviance
-  r <- summary(modelobject)$r.sq #r.squared
-  e<- summary(modelobject)$edf #EDF
-  a<- AIC(modelobject) #AIC
+  dev <- summary(modelobject)[14] #explained deviance
+  r2 <- summary(modelobject)$r.sq #r.squared
+  edf <- summary(modelobject)$edf #EDF
+  aic <- AIC(modelobject) #AIC
   p<- summary(modelobject)$s.table[,4] #p
-  out <- cbind(d,r,e,a,p)
+  out <- cbind(dev,r2,edf,aic,p)
   return(out)
 }
 
@@ -83,59 +83,61 @@ par(mfrow = c(2, 2))
 set.seed(26)
 
 #make GAMs, check, store output
-FvFm_mean_gam <- gam(FvFm_mean ~ s(Latitude, bs = "cs", k=9), method = "REML", data=d1)
-gam.check(FvFm_mean_gam) #passes
-(a <- lmp.2(FvFm_mean_gam))
+#FvFm
+FvFm_mean_gam <- gam(FvFm_mean ~ s(Latitude, k=9), select = T, method = "REML", data=d1)
+summary(FvFm_mean_gam)
+gam.check(FvFm_mean_gam, rep=500) 
+#plot(FvFm_mean_gam, residuals=TRUE,pch=19,cex=.3)
+(a <- lmp.2(FvFm_mean_gam)) #fits.
 
-qP_gam <- gam(qP ~ s(Latitude, bs = "cc", k=9), method = "REML", data=d1)
-gam.check(qP_gam)
-(b<- lmp.2(qP_gam)) #barely fit well 
+#qP
+qP_gam <- gam(qP ~ s(log(Latitude), k=9), method = "REML", data=d1)
+summary(qP_gam)
+gam.check(qP_gam, rep=500) #there seemed to be a log shaped curve spilling into the residuals, so I took the log of latitude to fit the model. it seemed to help?
+#plot(qP_gam, residuals=TRUE,pch=19,cex=.3)
+(b<- lmp.2(qP_gam)) #fits. 
 
-qL_gam <- gam(qL ~ s(Latitude, bs = "cs", k=9), method = "REML", data=d1)
-gam.check(qL_gam)
-(c <- lmp.2(qL_gam)) #is not fit well 
+#qL
+qL_gam <- gam(qL ~ s(log(Latitude), k=9), method = "REML", data=d1)
+summary(qL_gam)
+gam.check(qL_gam) #similar to qP, I took the log of latitude here and it seems to have helped the model. 
+#plot(qL_gam, residuals=TRUE,pch=19,cex=.3)
+(c <- lmp.2(qL_gam)) #fits.
 
-qN_gam <- gam(qN ~ s(Latitude, bs = "cs", k=9), method = "REML", data=d1)
-gam.check(qN_gam)
-(d <- lmp.2(qN_gam))
+#NPQ
+NPQ_gam <- gam(NPQ ~ s(Latitude, k=9), method = "REML", data=d1)
+summary(NPQ_gam)
+gam.check(NPQ_gam, rep=500)
+#plot(NPQ_gam, residuals=TRUE,pch=19,cex=.3)
+(d <- lmp.2(NPQ_gam)) #fits.
 
-NPQ_gam <- gam(NPQ ~ s(Latitude, bs = "cs", k=8), method = "REML", data=d1)
-gam.check(NPQ_gam)
-(e <- lmp.2(NPQ_gam)) #passable
+#phiPSII
+phiPSII_gam <- gam(phiPSII ~ s(Latitude, k=9), select =T, method = "REML", data=d1)
+summary(phiPSII_gam)
+gam.check(phiPSII_gam, rep=500) 
+#plot(phiPSII_gam, residuals = T, pch=19, cex=0.3) #fits.
+(e <- lmp.2(phiPSII_gam))
 
-phiPSII_gam <- gam(phiPSII ~ s(Latitude, k=9, bs = "cs" ), method = "REML", data=d1)
-gam.check(phiPSII_gam) #passes
-(f <- lmp.2(phiPSII_gam))
-f
-phiNPQ_gam <- gam(phiNPQ ~ s(Latitude, k=9, bs = "cs"), method = "REML", data=d1)
-gam.check(phiNPQ_gam) #passes
-(g <- lmp.2(phiNPQ_gam))
+#phiNPQ
+phiNPQ_gam <- gam(phiNPQ ~ s(Latitude, k=9), select = T, method = "REML", data=d1)
+summary(phiNPQ_gam)
+gam.check(phiNPQ_gam, rep=500) #passes
+#plot(phiNPQ_gam, residuals = T, pch=19, cex=0.3) #fits.
+(f <- lmp.2(phiNPQ_gam))
 
-phiNO_gam <- gam(phiNO ~ s(Latitude, bs = "cs", k=9), method = "REML", data=d1)
-gam.check(phiNO_gam) #passes
-(h <- lmp.2(phiNO_gam))
-
-JETR_gam <- gam(JETR ~ s(Latitude, bs = "cc", k=9), method = "REML", data=d1)
-gam.check(JETR_gam) #passes
-(i <- lmp.2(JETR_gam))
-
-JNPQ_gam <- gam(JNPQ ~ s(Latitude, bs = "bs", k=9), method = "REML", data=d1)
-gam.check(JNPQ_gam) #passes
-(j <- lmp.2(JNPQ_gam))
-
-JNO_gam <- gam(JNO ~ s(Latitude, bs = "cs", k=9), method = "REML", data=d1)
-gam.check(JNO_gam) #passes, tight call
-k <- lmp.2(JNO_gam)
-
-Jtot_gam <- gam(Jtot ~ s(Latitude, bs = "cs", k=9), method = "REML", data=d1)
-gam.check(Jtot_gam) #also broken. 
-l <- lmp.2(Jtot_gam)
+#phiNO
+phiNO_gam <- gam(phiNO_new ~ s(Latitude, k=9), method = "REML", data=d1)
+summary(phiNO_gam)
+gam.check(phiNO_gam, rep=500) #passes
+#plot(phiNO_gam, residuals = T, pch=19, cex=0.3) #fits.
+(g <- lmp.2(phiNO_gam))
 
 #combine
-(gam <- rbind(a,b,c,d,e,f,g,h,i,j,k,l))
+(gam <- rbind(a,b,c,d,e,f,g))
 
 #combine linear regression and GAM results
-ID <- c("FvFm","qP","qL","qN", "NPQ","phiPSII","phiNPQ","phiNO","JETR","JNPQ","JNO","Jtot")
+ID <- c("FvFm","qP","qL","NPQ","phiPSII","phiNPQ","phiNO")
+(tab.1 <- cbind(ID, gam))
 tab <- cbind(ID, lm, gam)
 
 #rename columns 
